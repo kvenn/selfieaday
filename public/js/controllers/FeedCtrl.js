285ac6f5a3acb39717e7daa7fa0faf3225be8fc8
Kyle Venn <kvenn33@gmail.com>
2015-04-20 22:22:24 -0400

    one working solution for image slideshow

--- public/js/controllers/FeedCtrl.js
+++ public/js/controllers/FeedCtrl.js
@@ -13,12 +13,24 @@ angular.module('feed', [])
 		$locationProvider.html5Mode(true);
 	}])
 	.controller('FeedController',
-	['$http', '$scope', '$routeParams',
-	 function ($http, $scope, $routeParams)
+	['$http', '$scope', '$routeParams', '$timeout',
+	 function ($http, $scope, $routeParams, $timeout)
 	 {
+		 // TODO pull out to global
+		 $scope.basePhotoUrl = "https://selfieaday.s3.amazonaws.com/";
+
 		 $http.get('/api/user')
 			 .success(function (data)
 			 {
 				 $scope.users = data;
+
+				 $scope.images = data[1].pics;
+
+				 $scope.imgIndex = 0;
+				 $timeout(function advanceSlide() {
+					 $scope.imgIndex = ($scope.imgIndex + 1) % $scope.images.length;
+					 $timeout(advanceSlide, 500);
+				 });
 			 });
+
 	 }]);
