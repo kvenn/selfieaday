bb85989b29e5e7c9b638f631b884d0a9f86ab854
Kyle Venn <kvenn33@gmail.com>
2015-04-25 14:13:06 -0400

    trying to fix upload

--- app/routes.js
+++ app/routes.js
@@ -164,6 +164,7 @@ module.exports = function (app, passport)
 		};
 		s3.getSignedUrl('putObject', s3_params, function (err, data)
 		{
+			console.log(data);
 			if (err)
 			{
 				console.log(err);
